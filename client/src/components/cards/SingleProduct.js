77730aebf26fe3ba319a94ba703ec5eec73e2ea6
Rahul Bera <rahulbera3554@gmail.com>
2024-12-06 07:57:04 +0530

    first commit

---
+++ client/src/components/cards/SingleProduct.js
@@ -0,0 +1,78 @@
+import React, { lazy } from "react";
+import { Card, Tabs } from "antd";
+import { Link } from "react-router-dom";
+import { HeartOutlined, ShoppingCartOutlined } from "@ant-design/icons";
+import { Carousel } from "react-responsive-carousel";
+import "./SingleProduct.css"
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import Laptop from '../../images/laptop.png';
+import ProductListItems from "./ProductListItems";
+import './SingleProduct.css';
+import StarRating from "react-star-ratings";
+import RatingModal from "../modal/RatingModal";
+import { showAverage } from "../../functions/rating";
+const { Meta } = Card;
+
+const { TabPane } = Tabs;
+const SingleProduct = ({ product , onStarClick , star}) => {
+  const { title, description, images, _id } = product;
+
+  return (
+    <>
+      <div className="col-md-7">
+        {images && images.length ? (
+          <Carousel showArrows={true} autoPlay infiniteLoop>
+            {images && images.map((i) => <img src={i.url} key={i.public_id} />)}
+          </Carousel>) : (
+          <Card cover={<img src={Laptop} className="mb-3 card-image" />}></Card>
+        )}
+
+        <Tabs type="card">
+          <TabPane tab="Description" key="1">
+            {description && description}
+          </TabPane>
+          <TabPane tab="More" key="2">
+            Call use on xxxx xxx xxx to learn more about this product.
+          </TabPane>
+        </Tabs>
+
+      </div>
+
+      <div className="col-md-5">
+        <h3 className="p-3" style={{ backgroundColor: "#D4BEE4" }}>{title}</h3>
+        {product && product.ratings && product.ratings.length > 0 ? (
+          showAverage(product)
+        ) : (
+          <div className="text-center pt-1 pb-3">No rating yet</div>
+        )}
+
+
+        <Card
+          actions={[
+            <>
+              <ShoppingCartOutlined className="text-success" /> <br />
+              Add to Cart
+            </>,
+            <Link to="/">
+              <HeartOutlined className="text-info" /> <br /> Add to Wishlist
+            </Link>,
+            <RatingModal>
+              <StarRating
+                name={_id}
+                numberOfStars={5}
+                rating={star}
+                changeRating={onStarClick}
+                isSelectable={true}
+                starRatedColor="red"
+              />
+            </RatingModal>,
+          ]}
+        >
+          <ProductListItems product={product} />
+        </Card>
+      </div>
+    </>
+  );
+};
+
+export default SingleProduct;
